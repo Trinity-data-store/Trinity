9be8c9487d04dda7f3ef543c39e281a0a5054cfd
Ziming Mao <ziming.mao@yale.edu>
2021-07-25 16:38:33 -0400

    fix rand and getitmestamp

--- libmdtrie/bench/concurrency_bench.cpp
+++ libmdtrie/bench/concurrency_bench.cpp
@@ -8,12 +8,13 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <rand_utils.h>
 
 const int DIMENSION = 3;
 level_t max_depth = 32;
 level_t trie_depth = 10;
 preorder_t max_tree_node = 1024;
-n_leaves_t n_points = 10000000;
+n_leaves_t n_points = 1000000;
 const uint8_t max_num_threads = 36;
 
 typedef unsigned long long int TimeStamp;
@@ -26,33 +27,29 @@ static TimeStamp GetTimestamp() {
 }
 
 // void test_concurrency(md_trie<DIMENSION> *mdtrie){
-void test_concurrency(int i){
+void test_concurrency(){
 
-    // fprintf(stderr, "start: thread %d\n", i);
     auto *mdtrie_1 = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_node);
     auto *leaf_point = new data_point<DIMENSION>();
     symbol_t range = pow(2, max_depth);
+    auto *rand_generator = new utils::rand_utils();
 
+    // TimeStamp start = GetTimestamp();
     for (n_leaves_t itr = 1; itr <= n_points; itr++) {
 
 
         for (dimension_t i = 0; i < DIMENSION; i++) {
-            point_t coordinate = (point_t) rand() % range;
+            point_t coordinate = (point_t) rand_generator->rand_uint64(0, range - 1);
             leaf_point->set_coordinate(i, coordinate);
         }
         
-        // if (itr % 3 == 0){
-        //     fprintf(stderr, "thread %d, start itr %ld\n", i, itr);
-        // }
-
         mdtrie_1->insert_trie(leaf_point, max_depth);
-
-        // if (itr % 3 == 0){
-        //     fprintf(stderr, "thread %d, end itr %ld\n", i, itr);
-        // }
-
     }
-    // fprintf(stderr, "end: thread %d\n", i);
+    // TimeStamp diff = GetTimestamp() - start;
+
+    // uint64_t msec = diff * 1000 / CLOCKS_PER_SEC;  
+    // fprintf(stderr, "Total time: %lld us, for total number of threads %d\n", diff, i);
+
     return;    
 }
 
@@ -64,12 +61,12 @@ void test_func(){
 void vector_insertion(){
 
     symbol_t range = pow(2, max_depth);
-
+    auto *rand_generator = new utils::rand_utils();
     std::vector<point_t> myVector;
+
     for (n_leaves_t itr = 1; itr <= n_points; itr++) {
         
-        // point_t coordinate = (point_t) rand() + 1;
-        myVector.push_back(itr + 1);
+        myVector.push_back((point_t) rand_generator->rand_uint64(0, range - 1));
     }    
 
 
@@ -87,7 +84,7 @@ int main() {
     // 36 concurrent threads are supported
     std::cout << n << " concurrent threads are supported.\n";
 
-    for (uint8_t num_threads = 1; num_threads <= max_num_threads; num_threads ++){
+    for (uint8_t num_threads = 1; num_threads <= max_num_threads; num_threads += 3){
         std::thread *t_array = new std::thread[num_threads];
 
         TimeStamp start = GetTimestamp();
@@ -96,10 +93,10 @@ int main() {
             // Works: 
             // t_array[i] = std::thread(test_func);
 
-            t_array[i] = std::thread(vector_insertion);
+            // t_array[i] = std::thread(vector_insertion);
 
             // Doesn't work as expected; throughput stays the same
-            // t_array[i] = std::thread(test_concurrency, i);
+            t_array[i] = std::thread(test_concurrency);
 
         }
 
@@ -108,11 +105,12 @@ int main() {
         }
         TimeStamp diff = GetTimestamp() - start;
 
-        uint64_t msec = diff * 1000 / CLOCKS_PER_SEC;
+        // uint64_t msec = diff * 1000 / CLOCKS_PER_SEC;
         uint64_t total_points = n_points * num_threads;
         
-        fprintf(stderr, "Total time to insert %ld points with %d threads: %ld us\n", total_points, num_threads, msec*1000);
-        fprintf(stderr, "Throughput: %f\n", (float) total_points / msec*1000);
+        // fprintf(stderr, "msec: %ld\n", msec * 1000);
+        fprintf(stderr, "Total time to insert %ld points with %d threads: %lld us\n", total_points, num_threads, diff);
+        fprintf(stderr, "Throughput: %f per us\n", (float) total_points / diff);
 
     }
 }
