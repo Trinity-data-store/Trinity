0f1e3e27933e6f03c55c8d98efcfd7ae8bd10c66
Anurag Khandelwal <anurag.khandelwal@yale.edu>
2021-07-25 17:23:52 -0400

    Add support for Jemalloc

--- cmake-modules/Dependencies.cmake
+++ cmake-modules/Dependencies.cmake
@@ -11,6 +11,11 @@ set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX} ${CMAKE_FIND_LIBR
 # Threads
 find_package(Threads REQUIRED)
 
+# Jemalloc
+include(JemallocExternal)
+set(HEAP_MANAGER_EP "jemalloc_ep")
+set(HEAP_MANAGER_LIBRARY ${JEMALLOC_LIBRARY})
+
 # If testing is enabled
 if (BUILD_TESTS)
 	# Catch2
