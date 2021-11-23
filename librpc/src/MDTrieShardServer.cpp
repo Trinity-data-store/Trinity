3880965a8f628378b81bdf60a43a150284080802
Ziming Mao <ziming.mao@yale.edu>
2021-11-23 09:08:40 -0800

    .

--- librpc/src/MDTrieShardServer.cpp
+++ librpc/src/MDTrieShardServer.cpp
@@ -39,9 +39,10 @@ public:
   MDTrieHandler(){
     
     mdtrie_ = new md_trie(max_depth, trie_depth, max_tree_node);
-    std::vector<level_t> dimension_bits = {24, 16, 8, 32, 24, 16, 32, 32}; // 8 Dimensions
-    std::vector<level_t> new_start_dimension_bits = {16, 8, 0, 24, 16, 0, 0, 0}; // 8 Dimensions
-
+    dimension_bits = {8, 32, 16, 24, 32, 32, 32, 32, 32}; // 9 Dimensions
+    new_start_dimension_bits = {0, 0, 8, 16, 0, 0, 0, 0, 0}; // 9 Dimensions
+    is_osm = true;
+    
     start_dimension_bits = new_start_dimension_bits;  
     create_level_to_num_children(dimension_bits, 32);
 
