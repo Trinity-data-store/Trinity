a8f4aca72f968a08917184cfc2242089f5900bdf
Ziming Mao <ziming.mao@yale.edu>
2022-01-01 08:57:47 -0800

    added mdtrie tests

--- libmdtrie/test/mdtrie_lookup_test.cpp
+++ libmdtrie/test/mdtrie_lookup_test.cpp
@@ -1,7 +1,126 @@
 #include "catch.hpp"
 #include "trie.h"
 
-TEST_CASE("Check Lookup", "[trie]") {
+const int DIMENSION = 10;
 
-    REQUIRE(true);
+bool test_lookup(n_leaves_t n_points, level_t max_depth, level_t trie_depth, preorder_t max_tree_node){
+
+    /**
+        First insert random points into the mdtrie
+        Lookup query every points in the mdtrie given primary keys
+    */   
+
+    create_level_to_num_children(std::vector<level_t>(DIMENSION, max_depth), std::vector<level_t>(DIMENSION, 0), max_depth);
+
+    auto range = (point_t) pow(2, max_depth);
+    auto *mdtrie = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_node);
+    data_point<DIMENSION> leaf_point;
+    std::vector<data_point<DIMENSION>> all_leaf_points;
+    point_t max[DIMENSION];
+    point_t min[DIMENSION];
+
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+
+        for (dimension_t i = 0; i < DIMENSION; i++) {
+            leaf_point.set_coordinate(i, rand() % range);
+            if (itr == 1) {
+                max[i] = leaf_point.get_coordinate(i);
+                min[i] = leaf_point.get_coordinate(i);
+            } else {
+                if (leaf_point.get_coordinate(i) > max[i]) {
+                    max[i] = leaf_point.get_coordinate(i);
+                }
+                if (leaf_point.get_coordinate(i) < min[i]) {
+                    min[i] = leaf_point.get_coordinate(i);
+                }
+            }
+        }
+        mdtrie->insert_trie(&leaf_point, itr - 1);
+        all_leaf_points.push_back(leaf_point);
+    }
+
+    for (n_leaves_t i = 0; i < n_points; i++){
+
+        point_t *node_path_from_primary = (point_t *)malloc((max_depth + 1) * sizeof(point_t));
+        tree_block<DIMENSION> *t_ptr = (tree_block<DIMENSION> *) (p_key_to_treeblock_compact.At(i));
+
+        point_t parent_symbol_from_primary = t_ptr->get_node_path_primary_key(i, node_path_from_primary);
+        node_path_from_primary[max_depth - 1] = parent_symbol_from_primary;
+
+        auto returned_coordinates = t_ptr->node_path_to_coordinates(node_path_from_primary, DIMENSION);
+
+        for (dimension_t j = 0; j < DIMENSION; j++){
+            if (returned_coordinates->get_coordinate(j) != all_leaf_points[i].get_coordinate(j)){
+                return false;
+            }
+        }    
+    }
+    delete mdtrie;
+    return true;    
+}
+
+
+bool test_lookup_close(n_leaves_t n_points, level_t max_depth, level_t trie_depth, preorder_t max_tree_node){
+
+    /**
+        First insert random points into the mdtrie 
+        Each attribute can only be from [0, 1023]
+        Lookup query every points in the mdtrie given primary keys
+    */   
+
+    create_level_to_num_children(std::vector<level_t>(DIMENSION, max_depth), std::vector<level_t>(DIMENSION, 0), max_depth);
+
+    auto range = (point_t) 1024;  // A close range
+    auto *mdtrie = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_node);
+    data_point<DIMENSION> leaf_point;
+    std::vector<data_point<DIMENSION>> all_leaf_points;
+    point_t max[DIMENSION];
+    point_t min[DIMENSION];
+
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+
+        for (dimension_t i = 0; i < DIMENSION; i++) {
+            leaf_point.set_coordinate(i, rand() % range);
+            if (itr == 1) {
+                max[i] = leaf_point.get_coordinate(i);
+                min[i] = leaf_point.get_coordinate(i);
+            } else {
+                if (leaf_point.get_coordinate(i) > max[i]) {
+                    max[i] = leaf_point.get_coordinate(i);
+                }
+                if (leaf_point.get_coordinate(i) < min[i]) {
+                    min[i] = leaf_point.get_coordinate(i);
+                }
+            }
+        }
+        mdtrie->insert_trie(&leaf_point, itr - 1);
+        all_leaf_points.push_back(leaf_point);
+    }
+
+    for (n_leaves_t i = 0; i < n_points; i++){
+
+        point_t *node_path_from_primary = (point_t *)malloc((max_depth + 1) * sizeof(point_t));
+        tree_block<DIMENSION> *t_ptr = (tree_block<DIMENSION> *) (p_key_to_treeblock_compact.At(i));
+
+        point_t parent_symbol_from_primary = t_ptr->get_node_path_primary_key(i, node_path_from_primary);
+        node_path_from_primary[max_depth - 1] = parent_symbol_from_primary;
+
+        auto returned_coordinates = t_ptr->node_path_to_coordinates(node_path_from_primary, DIMENSION);
+
+        for (dimension_t j = 0; j < DIMENSION; j++){
+            if (returned_coordinates->get_coordinate(j) != all_leaf_points[i].get_coordinate(j)){
+                return false;
+            }
+        }    
+    }
+    delete mdtrie;
+    return true;    
+}
+
+TEST_CASE("Check Lookup", "[trie]") {
+    REQUIRE(test_lookup(1000000, 10, 3, 128));
+}
+
+TEST_CASE("Check Lookup Close", "[trie]") {
+    REQUIRE(test_lookup_close(1000000, 10, 3, 128));
 }
--- libmdtrie/test/mdtrie_test.cpp
+++ libmdtrie/test/mdtrie_test.cpp
@@ -1,14 +1,104 @@
 #include "catch.hpp"
 #include "trie.h"
 
+const int DIMENSION = 10;
+
+bool test_random_data(n_leaves_t n_points, level_t max_depth, level_t trie_depth,
+                      preorder_t max_tree_nodes) {
+
+    /**
+        Insert random points into the mdtrie
+        Check whether the inserted points exist
+    */     
+
+    create_level_to_num_children(std::vector<level_t>(DIMENSION, max_depth), std::vector<level_t>(DIMENSION, 0), max_depth);
+    auto range = (n_leaves_t) pow(2, max_depth);
+    auto *mdtrie = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_nodes);
+    auto *leaf_point = new data_point<DIMENSION>();
+
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+        for (dimension_t i = 0; i < DIMENSION; i++) {
+            leaf_point->set_coordinate(i, (point_t) rand() % range);
+        }
+        mdtrie->insert_trie(leaf_point, itr - 1);
+        if (!mdtrie->check(leaf_point)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool test_nonexistent_data(n_leaves_t n_points, level_t max_depth, level_t trie_depth,
+                           preorder_t max_tree_node) {
+
+    /**
+        Insert random points into the mdtrie
+        Query non-existent points
+        Pass the test if none of those points can be found
+    */ 
+
+    create_level_to_num_children(std::vector<level_t>(DIMENSION, max_depth), std::vector<level_t>(DIMENSION, 0), max_depth);
+    auto range = (n_leaves_t) pow(2, max_depth);
+    auto *mdtrie = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_node);
+    auto *leaf_point = new data_point<DIMENSION>();
+
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+        for (dimension_t i = 0; i < DIMENSION; i++) {
+            leaf_point->set_coordinate(i, rand() % (range / 2));
+        }
+        mdtrie->insert_trie(leaf_point, itr - 1);
+    }
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+        for (dimension_t i = 0; i < DIMENSION; i++) {
+            leaf_point->set_coordinate(i, rand() % (range / 2) + range / 2);
+        }
+        if (mdtrie->check(leaf_point)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool test_contiguous_data(n_leaves_t n_points, level_t max_depth, level_t trie_depth,
+                          preorder_t max_tree_node) {
+
+    /**
+        Insert random points that are close to each other into the mdtrie
+        Check whether the inserted points exist
+    */ 
+
+    create_level_to_num_children(std::vector<level_t>(DIMENSION, max_depth), std::vector<level_t>(DIMENSION, 0), max_depth);
+    auto range = (n_leaves_t) pow(2, max_depth);
+    auto *mdtrie = new md_trie<DIMENSION>(max_depth, trie_depth, max_tree_node);
+    auto *leaf_point = new data_point<DIMENSION>();
+
+    for (n_leaves_t itr = 1; itr <= n_points; itr++) {
+
+        auto first_half_value = (point_t) rand() % range;
+        for (dimension_t i = 0; i < DIMENSION / 2; i++) {
+            leaf_point->set_coordinate(i, first_half_value);
+        }
+        auto second_half_value = (point_t) rand() % range;
+        for (dimension_t i = DIMENSION / 2; i < DIMENSION; i++) {
+            leaf_point->set_coordinate(i, second_half_value);
+        }
+        mdtrie->insert_trie(leaf_point, itr - 1);
+        if (!mdtrie->check(leaf_point)) {
+            raise(SIGINT);
+            return false;
+        }
+    }
+    return true;
+}
+
 TEST_CASE("Check Random Data Insertion", "[trie]") {
-    REQUIRE(true);
+    REQUIRE(test_random_data(100000, 20, 10, 1024));
 }
 
 TEST_CASE("Check Nonexistent Data", "[trie]") {
-    REQUIRE(true);
+    REQUIRE(test_nonexistent_data(50000, 20, 10, 1024));
 }
 
 TEST_CASE("Check Contiguous Data", "[trie]") {
-    REQUIRE(true);
+    REQUIRE(test_contiguous_data(50000, 10, 3, 128));
 }
