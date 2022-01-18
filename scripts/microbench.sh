40f51808e00440eaed7cdfe7fa0aed0ba42b84bb
Ziming Mao <ziming.mao@yale.edu>
2022-01-18 14:23:16 -0800

    minor change to bench file

--- scripts/microbench.sh
+++ scripts/microbench.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 cd /home/ziming/md-trie/build/
+make
+NUM_REPEATS=10
 
-NUM_REPEATS=1
+# for i in $( seq 1 $NUM_REPEATS )
+# do
+#     echo "**** fs_bench $i's iteration started! ****" >> ../results_log/microbench_fs.log
+#     libmdtrie/fs_bench >> ../results_log/microbench_fs.log
+#     echo "\n" >> ../results_log/microbench_fs.log
+# done
 
-for i in $( seq 1 $NUM_REPEATS )
-do
-    echo "**** fs_bench $i's iteration started! ****" >> ../results_log/microbench_fs.log
-    libmdtrie/fs_bench >> ../results_log/microbench_fs.log
-    echo "\n" >> ../results_log/microbench_fs.log
-done
-
-for i in $( seq 1 $NUM_REPEATS )
+for i in $( seq 7 $NUM_REPEATS )
 do
     echo "**** osm_bench  $i's iteration started! ****" >> ../results_log/microbench_osm.log
     libmdtrie/osm_bench >> ../results_log/microbench_osm.log
     echo "\n" >> ../results_log/microbench_osm.log
 done
 
-for i in $( seq 1 $NUM_REPEATS )
+for i in $( seq 7 $NUM_REPEATS )
 do
     echo "**** tpch_bench  $i's iteration started! ****" >> ../results_log/microbench_tpch.log
     libmdtrie/tpch_bench >> ../results_log/microbench_tpch.log
