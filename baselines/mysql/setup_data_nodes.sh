d6234f272a36799cbb3f2d2ec430bb8ab0d2ea64
Ziming Mao <ziming.mao@yale.edu>
2022-01-25 01:05:23 +0000

    added mysql data node setup script

--- baselines/mysql/setup_data_nodes.sh
+++ baselines/mysql/setup_data_nodes.sh
@@ -1,8 +1,10 @@
 sudo pkill -f ndbd
-sudo rm -r /usr/local/mysql/data
+# sudo rm -r /usr/local/mysql/data
 sudo mkdir -p /usr/local/mysql/data
+# sudo cp /usr/share/doc/util-linux/examples/securetty /etc/securetty
 sudo ndbd
-sudo cp ndbd.service /etc/systemd/system/ndbd.service
+sudo pkill -f ndbd
+# sudo cp ndbd.service /etc/systemd/system/ndbd.service
 sudo systemctl daemon-reload
 sudo systemctl enable ndbd
 sudo systemctl start ndbd
