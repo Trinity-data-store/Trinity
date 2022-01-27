d35b7d745bb0cf268afe8e46f5f1fc5e2dbe01e4
Ziming Mao <ziming.mao@yale.edu>
2022-01-27 10:06:22 -0500

    setup manager script

--- baselines/mysql/setup_manager.sh
+++ baselines/mysql/setup_manager.sh
@@ -2,7 +2,7 @@
 sudo pkill -f ndb_mgmd
 sudo rm -r /var/lib/mysql-cluster
 sudo mkdir -p /var/lib/mysql-cluster
-# sudo cp config.ini /var/lib/mysql-cluster/config.ini
+sudo cp config.ini /var/lib/mysql-cluster/config.ini
 sudo ndb_mgmd --initial --config-file=/var/lib/mysql-cluster/config.ini
 sudo pkill -f ndb_mgmd
 # sudo cp ndb_mgm.service /etc/systemd/system/ndb_mgmd.service
