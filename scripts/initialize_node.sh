a9214bc2670903721c7bddd8a170918eb5b3008c
Ziming <Ziming@node0.ziming-121692.trinity-pg0.utah.cloudlab.us>
2022-03-25 20:23:42 -0600

    comment out

--- scripts/initialize_node.sh
+++ scripts/initialize_node.sh
@@ -5,10 +5,10 @@ git config --global user.email "ziming.mao@yale.edu"
 
 cd /mntData2/
 sudo apt update
-wget https://github.com/Kitware/CMake/releases/download/v3.23.0-rc5/cmake-3.23.0-rc5-linux-x86_64.tar.gz
+# wget https://github.com/Kitware/CMake/releases/download/v3.23.0-rc5/cmake-3.23.0-rc5-linux-x86_64.tar.gz
 
 # Install cmake
-tar -xzf cmake-3.23.0-rc5-linux-x86_64.tar.gz
+# tar -xzf cmake-3.23.0-rc5-linux-x86_64.tar.gz
 sudo cp -r cmake-3.23.0-rc5-linux-x86_64/* /usr 
 PATH=/usr/:$PATH
 
@@ -24,20 +24,20 @@ sudo apt-get install -y libevent-dev
 
 #libevent
 cd /mntData2/
-wget https://github.com/libevent/libevent/releases/download/release-2.1.10-stable/libevent-2.1.10-stable.tar.gz
-tar xfz libevent-2.1.10-stable.tar.gz
+# wget https://github.com/libevent/libevent/releases/download/release-2.1.10-stable/libevent-2.1.10-stable.tar.gz
+# tar xfz libevent-2.1.10-stable.tar.gz
 cd libevent-2.1.10-stable
-sudo ./configure --prefix=/usr 
-sudo make
+# sudo ./configure --prefix=/usr 
+# sudo make
 sudo make install
 cd /mntData2/
 
 # Install thrift
-git clone -b 0.15.0 https://github.com/apache/thrift.git
+# git clone -b 0.15.0 https://github.com/apache/thrift.git
 cd thrift
-./bootstrap.sh
-sudo ./configure
-sudo make
+# ./bootstrap.sh
+# sudo ./configure
+# sudo make
 sudo make install
 cd /mntData2/
 
