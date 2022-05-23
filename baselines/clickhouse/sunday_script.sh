88c9b64d9f3174c55e93190fe10387f80223a656
MaoZiming <ziming.mao@yale.edu>
2022-05-22 21:43:40 -0400

    T1

--- baselines/clickhouse/sunday_script.sh
+++ baselines/clickhouse/sunday_script.sh
@@ -1,9 +1,11 @@
-cd /proj/trinity-PG0/Trinity/baselines/clickhouse
+cd /proj/trinity-PG0/Trinity/build
+make -j
 
-python3 query_new.py 2 122 >> query_tpch_T2_range0.10_new
-python3 re_run_query.py query_tpch_T2_range0.10_new >> query_tpch_T2_range0.10_rerun
-python3 convert_query_format.py query_tpch_T2_range0.10_rerun
+base_dir=../baselines/clickhouse
 
-python3 query_new.py 5 198 >> query_tpch_T5_range0.10
-python3 re_run_query.py query_tpch_T5_range0.10 >> query_tpch_T5_range0.10_rerun
-python3 convert_query_format.py query_tpch_T5_range0.10_rerun
+for i in 2 3
+do
+    echo $base_dir/query_tpch_T"$i"_range0.10_rerun_converted
+    echo $base_dir/query_tpch_T"$i"_range0.10_trinity
+    librpc/TrinityTPCHMacro 1 $base_dir/query_tpch_T"$i"_range0.10_rerun_converted $base_dir/query_tpch_T"$i"_range0.10_trinity
+done
