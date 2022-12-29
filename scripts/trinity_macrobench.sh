bcf320d105bf4f6b325d0ae6f38080d050aaf654
MaoZiming <ziming.mao@yale.edu>
2022-12-28 22:07:05 -0700

    update results

--- scripts/trinity_macrobench.sh
+++ scripts/trinity_macrobench.sh
@@ -7,37 +7,37 @@ NYC=3
 cd /proj/trinity-PG0/Trinity/build/
 make
 
-echo "*** TPCH ***"
-bash /proj/trinity-PG0/Trinity/scripts/trinity_start.sh $TPCH
-sleep 20
+# echo "*** TPCH ***"
+# bash /proj/trinity-PG0/Trinity/scripts/trinity_start.sh $TPCH
+# sleep 20
 
-echo "*** TPCH insert ***"
-for i in {1..9}
-do
-    ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b insert" &
-done
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b insert
-sleep 60
+# echo "*** TPCH insert ***"
+# for i in {1..9}
+# do
+#     ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b insert" &
+# done
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b insert
+# sleep 60
 
-echo "*** TPCH lookup ***"
-for i in {1..9}
-do
-    ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b lookup" &
-done
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b lookup
-sleep 60
+# echo "*** TPCH lookup ***"
+# for i in {1..9}
+# do
+#     ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b lookup" &
+# done
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b lookup
+# sleep 60
 
-echo "*** TPCH query latency ***"
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b query_latency
-sleep 20
+# echo "*** TPCH query latency ***"
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b query_latency
+# sleep 20
 
-echo "*** TPCH lookup throughput ***"
-for i in {1..9}
-do
-    ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b query_throughput" &
-done
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b query_throughput
-sleep 60
+# echo "*** TPCH lookup throughput ***"
+# for i in {1..9}
+# do
+#     ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b query_throughput" &
+# done
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b query_throughput
+# sleep 60
 
 
 echo "*** TPCH lookup mixed ***"
@@ -48,24 +48,24 @@ done
 /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b lookup_mixed
 sleep 60
 
-echo "*** TPCH search mixed ***"
-for i in {1..1} # It will otherwise overwhelm the server
-do
-    ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b search_mixed" &
-done
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b search_mixed
-sleep 60
+# echo "*** TPCH search mixed ***"
+# for i in {1..1} # It will otherwise overwhelm the server
+# do
+#     ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b search_mixed" &
+# done
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b search_mixed
+# sleep 60
 
-echo "*** TPCH insert mixed ***"
-for i in {1..9}
-do
-    ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b insert_mixed" &
-done
-/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b insert_mixed
-sleep 60
+# echo "*** TPCH insert mixed ***"
+# for i in {1..9}
+# do
+#     ssh -o StrictHostKeyChecking=no -i /proj/trinity-PG0/Trinity/scripts/key -l Ziming 10.10.1.$(($i + 2)) "/proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d $i -b insert_mixed" &
+# done
+# /proj/trinity-PG0/Trinity/build/librpc/TrinityTPCH -d 0 -b insert_mixed
+# sleep 60
 
-bash /proj/trinity-PG0/Trinity/scripts/trinity_end.sh
-sleep 20
+# bash /proj/trinity-PG0/Trinity/scripts/trinity_end.sh
+# sleep 20
 
 # echo "*** Github ***"
 # bash /proj/trinity-PG0/Trinity/scripts/trinity_start.sh $GITHUB
