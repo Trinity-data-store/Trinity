ade921165f742fea89718d627f7713bd2f032170
MaoZiming <ziming.mao@yale.edu>
2024-02-15 00:05:54 -0700

    remove baselines

--- scripts/clang_format.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find ./libmdtrie -name *.h -o -name *.cpp -o -name *.hpp | xargs clang-format -i -style Mozilla
-find ./librpc -name *.h -o -name *.cpp -o -name *.hpp | xargs clang-format -i -style Mozilla
