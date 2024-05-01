332f97ef742705037e07295eaa2a33de712200c9
MaoZiming <ziming.mao@yale.edu>
2024-05-01 09:01:55 +0000

    update setup script

--- scripts/setup_one_node.sh
+++ scripts/setup_one_node.sh
@@ -10,32 +10,26 @@ dependencies_path="/proj/trinity-PG0/dependencies"
 
 # Basic setup
 sudo apt update
-# sudo apt upgrade
 sudo apt install -y htop
 sudo apt install -y dstat
 sudo apt install -y pkgconf  # making thrift requires this
-cd $dependencies_path
+sudo apt install -y cmake
+sudo apt install -y build-essential
+sudo apt install -y libboost-test-dev  
+sudo apt install -y  libboost-all-dev
+sudo apt install -y  libssl-dev libcurl4-openssl-dev
+sudo apt install -y libboost-thread-dev
+sudo apt install -y libbz2-dev
+sudo apt install -y libevent-dev
+sudo apt install -y clang-format
+sudo apt install -y flex bison
+sudo apt install -y npm
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs            
+sudo apt install -y ant 
+sudo apt install -y libatk1.0-dev libatk-bridge2.0-dev
+sudo apt-get install libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf-2-4 libasound2 libpango1.0-0 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libgdk-pixbuf2.0-0 libpangocairo-1.0-0
 
-# Install Cmake
-if [ ! -d "cmake-3.23.0-rc5-linux-x86_64" ]; then
-    wget -nc https://github.com/Kitware/CMake/releases/download/v3.23.0-rc5/cmake-3.23.0-rc5-linux-x86_64.tar.gz
-    tar -xvf cmake-3.23.0-rc5-linux-x86_64.tar.gz
-fi
-sudo cp -r cmake-3.23.0-rc5-linux-x86_64/* /usr 
-PATH=/usr/:$PATH
-cd $dependencies_path
-
-# Install libevent
-if [ ! -d "libevent-2.1.10-stable" ]; then
-    wget -nc  https://github.com/libevent/libevent/releases/download/release-2.1.10-stable/libevent-2.1.10-stable.tar.gz
-    tar -xvf libevent-2.1.10-stable.tar.gz
-    cd libevent-2.1.10-stable
-    sudo ./configure --prefix=/usr 
-    sudo make -j
-    cd ..
-fi
-cd libevent-2.1.10-stable
-sudo make install
 cd $dependencies_path
 
 # Install thrift
@@ -53,14 +47,3 @@ cd thrift
 sudo make install
 cd $dependencies_path
 
-# Install Other Packages for Trinity
-sudo apt-get install -y libboost-test-dev  
-sudo apt-get install -y  libboost-all-dev
-sudo apt-get install -y  curl
-sudo apt-get install -y  libssl-dev libcurl4-openssl-dev
-sudo apt install -y libboost-thread-dev
-sudo apt-get install -y libbz2-dev
-sudo apt-get install -y python3-dev  # for python3.x installs
-sudo apt-get install -y libevent-dev
-sudo apt-get install -y clang-format
-sudo apt-get install -y flex bison
